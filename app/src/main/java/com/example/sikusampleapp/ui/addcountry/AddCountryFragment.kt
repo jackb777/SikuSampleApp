4212373eb7d7145d570c511c69f17b60f742e330
jack-bonser <jack-bonser@hotmail.com>
2024-07-17 20:21:47 -0700

    added onclick functionalilty

--- app/src/main/java/com/example/sikusampleapp/ui/addcountry/AddCountryFragment.kt
+++ app/src/main/java/com/example/sikusampleapp/ui/addcountry/AddCountryFragment.kt
@@ -1,13 +1,20 @@
 package com.example.sikusampleapp.ui.addcountry
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.TextView.BufferType.SPANNABLE
 import androidx.fragment.app.Fragment
 import com.example.sikusampleapp.R
 import com.example.sikusampleapp.databinding.FragmentAddCountryBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class AddCountryFragment : Fragment(R.layout.fragment_add_country) {
@@ -31,6 +38,21 @@ class AddCountryFragment : Fragment(R.layout.fragment_add_country) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        with(binding) {
+            addCountryButton.setOnClickListener {
+                updateText()
+            }
+        }
+    }
 
+    private fun updateText() {
+        val text = binding.addCountryTitle.text
+        val builder = SpannableStringBuilder()
+        val spannable = SpannableString(text)
+        spannable.setSpan(ForegroundColorSpan(Color.BLUE), 7, 12, 0)
+        spannable.setSpan(ForegroundColorSpan(Color.BLUE), 24, text.length, 0)
+        builder.append(spannable)
+
+        binding.addCountryTitle.setText(builder, SPANNABLE)
     }
 }
